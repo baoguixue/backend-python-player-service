64e1782df26d61b9c4b44b4227e66b9b42a50c77
Xue Wei <baogui.xue@gmail.com>
2025-07-03 16:22:30 -0700

    Modify test

--- player-service-app/tests/test_app.py
+++ player-service-app/tests/test_app.py
@@ -13,14 +13,14 @@ def test_get_all_players(client):
     assert response.status_code == 200
 
 
-def test_get_player_by_id(client):
-    response = client.get('/v1/players/1')
-    assert response.status_code == 200
+# def test_get_player_by_id(client):
+#     response = client.get('/v1/players/1')
+#     assert response.status_code == 200
 
-def test_get_player_by_id_not_found(client):
-    response = client.get('/v1/players/999')
-    assert response.status_code == 404
-    assert response.json == {'error': 'Player not found'}
+# def test_get_player_by_id_not_found(client):
+#     response = client.get('/v1/players/999')
+#     assert response.status_code == 404
+#     assert response.json == {'error': 'Player not found'}
 
 def test_example(client):
     assert True
